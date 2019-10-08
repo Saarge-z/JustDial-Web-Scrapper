0b06e531542d35cb926778860084716df59acbda
Saarge <razamohib01@gmail.com>
2019-10-08 21:52:15 +0530

    Second commit

--- scrap.py
+++ scrap.py
@@ -1,16 +1,22 @@
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-import requests
 
-source = requests.get('https://www.justdial.com/Aligarh/Restaurant-Collections/AllCategory').text
+url = 'https://www.justdial.com/Delhi/Ardor-Restaurant-Lounge-Terrace-Cafe-Connaught-Place/011PXX11-XX11-121106171304-R3S5_BZDET?srcterm=Ardor%2520Restaurant%2520Lounge%2520Terrace%2520Cafe'
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-soup = BeautifulSoup(source, 'lxml')
+webpage = urlopen(req)
+
+soup = BeautifulSoup(webpage)
+print(soup)
+
+
 
 #first_div = soup.find('div', class_ = 'mlistviewpr')
 
-first_div = soup.find('div', class_='holder')
+#first_div = soup.find('div', class_='holder')
 # sec_div = first_div.find('div', class_="wrapper filter-section")
 # thrid_div = sec_div.find('div')
 # anchr = thrid_div.find('div', class_ = 'mlistviewpr').a
 #name = anchr.find('span', class_ = 'meditle lng_commn').text
 
-print(name)
+#print(name)
