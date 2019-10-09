4aba04442d85efebfae28cffd1f13fa8ec1ac671
Saarge <razamohib01@gmail.com>
2019-10-10 01:15:00 +0530

    pushing code in single script

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Web_scrap1
-Web scrapping test
+Web scrapping
 
 Python Packages
 
-    Beautifulsoup
-    Image
-    numpy
+Use pip as a package manager for installing these packages
+    from urllib.request import urlopen, Request
+    from bs4 import BeautifulSoup
+
+    from selenium import webdriver
+    from selenium.webdriver.firefox.options import Options
+    from PIL import Image
--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from PIL import Image
+import os
 
 options = Options()
 options.add_argument("--headless")
 # options.add_argument('--ignore-certificate-errors')
 # options.add_argument("--test-type")
-#options.binary_location = "/usr/bin/chromium"
+# options.binary_location = "/usr/bin/chromium"
 driver = webdriver.Firefox()
 
 url = 'https://www.justdial.com/Delhi/Ardor-Restaurant-Lounge-Terrace-Cafe-Connaught-Place/011PXX11-XX11-121106171304-R3S5_BZDET?srcterm=Ardor%2520Restaurant%2520Lounge%2520Terrace%2520Cafe'
@@ -13,3 +15,15 @@ driver.get(url)
 driver.save_screenshot("screenshot.png")
 print (driver.title)
 driver.close()
+
+
+def crop(png_image_name):
+    img = Image.open(png_image_name)
+    area = (160, 220, 400, 300)
+    cropped_img = img.crop(area)
+    cropped_img.save(png_image_name)
+    cropped_img.show()
+
+for f in os.listdir('.'):
+    if f.endswith('.png'):
+        crop(f)
